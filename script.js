3b60a6b9bd1f1af6138865cba876f70117f1144d
Alexandra Marisa Cota <alexandracota@calvisitor-10-105-144-59.calvisitor.1918.berkeley.edu>
2017-05-10 21:15:52 -0700

    push to origin

--- script.js
+++ script.js
@@ -9,52 +9,90 @@ var guessesLeft = 10;
 var guessesSoFar = 0;
 
 
+var generateRandom = function() {
+	
+	return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+}
+
+var letter = "";
+
 //When the user presses a key, it will run the following funtion.
 document.onkeyup = function(event) {
+
+	if (guessesLeft === 10) {
+		letter = generateRandom();
+
+	}
+
+	guessesLeft--;
+
+	console.log(letter);
 
 	//Determine which key was pressed.
 	var userGuess = event.key;
 
-	//Here the computer chooses a random letter.
-	var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
-	console.log(computerGuess);
-
-	//for loop???
-	for (i = 0; i > guessesLeft.length; i--) {
-		
-		//Add to guessesSoFar score.
-		guessesSoFar++;
-		
-		//Subtract from guessesLeft score.
-		guessesLeft--;
-
-
-	//If user guesses the computer's letter...
-
-	if (userGuess === computerGuess) {
+	if (event.key === letter) {
 		wins++;
-
-		//Grab element by ID and rewrite the paragraph.
-		document.getElementById("wins");
-		document.innerHTML() = "Wins: " + this.wins;
-			
-		}
-
-	//If user guesses the incorrect letter...
-	else {
-		losses++;
-		document.getElementById("losses");
-		document.innerHTML() = "Losses: " + this.losses;
-		
-
-	console.log(userWin);
+	} else {
+		guessesSoFar++;
 	}
 
-	//if userGuess
+
+	if (guessesLeft === 0) {
+		letter = generateRandom();
+		losses++;
+		guessesLeft = 10;
+		guessesSoFar = 0;
+
+	}
+
+
+	
+
+
+
+
+
+	//Here the computer chooses a random letter.
+	
+
+	//console.log(computerGuess);
+
+	//for loop???
+	// for (i = 0; i > guessesLeft.length; i--) {
+		
+	// 	//Add to guessesSoFar score.
+	// 	guessesSoFar++;
+		
+		
+
+
+	// //If user guesses the computer's letter...
+
+	// if (userGuess === computerGuess) {
+
+
+	// 	//Grab element by ID and rewrite the paragraph.
+	// 	document.getElementById("wins");
+	// 	document.innerHTML() = "Wins: " + wins;
+			
+	// 	}
+
+	// //If user guesses the incorrect letter...
+	// else {
+	// 	losses++;
+	// 	document.getElementById("losses");
+	// 	document.innerHTML() = "Losses: " + losses;
+		
+
+	// console.log(userWin);
+	// }
+
+	// //if userGuess
 
 };
-};
+
 
 
 
